c5b6e161a0071eddd5341951c369d5ff8e69d34e
shazid25 <ishazid57@gmail.com>
2025-11-15 21:28:26 +0600

    Basic setup of project layout is done

---
+++ src/components/pages/educatorPage/MyCourses.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyCourses = () => {
+  return (
+    <div>
+      <h2>Teachers MyCourses</h2>
+    </div>
+  )
+}
+
+export default MyCourses
---
+++ src/components/pages/studentPage/CousersesList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CousersesList = () => {
+  return (
+    <div>
+      <h2>CousersesList</h2>
+    </div>
+  )
+}
+
+export default CousersesList
