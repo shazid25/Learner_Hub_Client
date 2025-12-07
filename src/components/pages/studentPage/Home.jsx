7bca70e03f32229c04fef63a689b78587d336f59
shazid25 <ishazid57@gmail.com>
2025-12-07 23:47:56 +0600

    Working on UI

--- src/components/pages/studentPage/Home.jsx
+++ src/components/pages/studentPage/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from '../../student/Hero';
+import Companies from '../../student/Companies';
+import CoursesSection from '../../student/CoursesSection';
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
       <Hero></Hero>
+      <Companies></Companies>
+      <CoursesSection></CoursesSection>
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        'course-details-heading-small': ['26px', { lineHeight: '36px' }],
+        'course-details-heading-large': ['36px', { lineHeight: '44px' }],
+        'home-heading-small': ['28px', { lineHeight: '34px' }],
+        'home-heading-large': ['48px', { lineHeight: '56px' }],
+        'default': ['15px', { lineHeight: '21px' }]
+      }
+    },
+  },
+  plugins: [],
+}
